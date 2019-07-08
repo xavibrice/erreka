23725ba3d1703ba6ee3955654820cbfda8ad5453
Xavier Briceño <infoloyaltylabel@gmail.com>
2019-07-08 19:36:29 +0200

    Create CRUD Company Admin

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+const $ = require('jquery');
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+require('startbootstrap-sb-admin-2/vendor/fontawesome-free/css/all.min.css');
+require('startbootstrap-sb-admin-2/css/sb-admin-2.css');
+require('startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.css');
+
+
+require('startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min');
+require('startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min');
+require('startbootstrap-sb-admin-2/js/sb-admin-2.min');
+require('startbootstrap-sb-admin-2/vendor/datatables/jquery.dataTables.min');
+require('startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4.min');
+require('startbootstrap-sb-admin-2/js/demo/datatables-demo');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
