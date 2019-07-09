ac8b04cbcaa1a4b90a072d8fdb53579c86103159
Xavier Briceño <infoloyaltylabel@gmail.com>
2019-07-09 14:16:32 +0200

    Crud Owner and Zone

---
+++ src/Controller/ZoneController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Zone;
+use App\Form\ZoneType;
+use App\Repository\ZoneRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/zone")
+ */
+class ZoneController extends AbstractController
+{
+    /**
+     * @Route("/", name="zone_index", methods={"GET"})
+     */
+    public function index(ZoneRepository $zoneRepository): Response
+    {
+        return $this->render('zone/index.html.twig', [
+            'zones' => $zoneRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="zone_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $zone = new Zone();
+        $form = $this->createForm(ZoneType::class, $zone);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($zone);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('zone_index');
+        }
+
+        return $this->render('zone/new.html.twig', [
+            'zone' => $zone,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="zone_show", methods={"GET"})
+     */
+    public function show(Zone $zone): Response
+    {
+        return $this->render('zone/show.html.twig', [
+            'zone' => $zone,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="zone_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Zone $zone): Response
+    {
+        $form = $this->createForm(ZoneType::class, $zone);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('zone_index', [
+                'id' => $zone->getId(),
+            ]);
+        }
+
+        return $this->render('zone/edit.html.twig', [
+            'zone' => $zone,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="zone_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Zone $zone): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$zone->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($zone);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('zone_index');
+    }
+}
---
+++ src/Form/OwnerType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Owner;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OwnerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('first_name')
+            ->add('last_name')
+            ->add('email')
+            ->add('phone')
+            ->add('mobile')
+            ->add('news')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Owner::class,
+        ]);
+    }
+}
