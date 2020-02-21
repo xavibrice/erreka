fc13da96b7f182dd93ea81ed7431e400293dfd89
Xavier Briceño <infoloyaltylabel@gmail.com>
2020-02-21 18:35:22 +0100

    situation leng 150

--- src/Entity/Situation.php
+++ src/Entity/Situation.php
@@ -22,7 +22,7 @@ class Situation
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @ORM\Column(type="string", length=150, unique=true)
      */
     private $name;
 
