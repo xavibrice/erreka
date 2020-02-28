eb3882a0351416b2ced469f82c7dca4b7d3aa5af
Xavier Briceño <infoloyaltylabel@gmail.com>
2020-02-28 19:59:29 +0100

    select provisional en visitas

--- src/Form/VisitNewType.php
+++ src/Form/VisitNewType.php
@@ -6,6 +6,7 @@ use App\Entity\Client;
 use App\Entity\Property;
 use App\Entity\Visit;
 use App\Form\Type\DatePickerType;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -32,6 +33,15 @@ class VisitNewType extends AbstractType
                 'placeholder' => 'Selecciona una propiedad',
                 'class' => Property::class,
                 'label' => 'Propiedad',
+                'query_builder' => function(EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('p')
+                        ->innerJoin('p.charge', 'c')
+                        ->innerJoin('c.charge_type', 'ct')
+                        ->andWhere('ct.name = :name')
+                        ->setParameter('name', 'autorizacion')
+                        ->setParameter('name', 'exclusiva')
+                        ;
+                }
             ])
             ->add('client', EntityType::class, [
                 'label' => 'Cliente',
